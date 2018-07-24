03e4179b63daec28142febf1d0ba7cf87df32bf8
Nicholas Hinds <hindsn@gmail.com>
2018-07-23 21:09:23 -0700

    Remove dead commented-out code from parallel_packages type

--- lib/puppet/type/parallel_packages.rb
+++ lib/puppet/type/parallel_packages.rb
@@ -30,31 +30,4 @@ Puppet::Type.newtype(:parallel_packages) do
     end
     []
   end
-
-  # autorequire(:package) do
-  #   Puppet.debug("Normal autorequire for #{self[:name]}")
-  #   []
-  # end
-  #
-#   # # Monkey patch puppet to allow arbitrary automatic relationships
-#   # # TODO: see if we can do this in some other way via the API
-#   # def autorequire(rel_catalog = nil)
-#   #   super.tap do |relationships|
-#   #     Puppet.debug "Thinking about autorequires for #{self[:name]}"
-#       # rel_catalog ||= catalog
-#   def add_package_relationships
-#     Array(self[:packages]).each do |package|
-#       package_resource = catalog.resource(:package, package)
-#       # Parallel_packages comes before the packages in its +packages+ property which are already in the catalog
-#       catalog.relationship_graph.add_edge(Puppet::Relationship.new(self, package_resource))
-#       # Parallel_packages comes after the requirements of the package resources in the catalog
-#       package_resource.builddepends
-#       # FIXME: damn, RelationshipGraph is technically private, but #relationship_graph is public?
-#       catalog.relationship_graph.direct_dependencies_of(package_resource).each do |dependency|
-#         Puppet.debug "Adding dependency from package[#{package}] to #{dependency.name}"
-#         relationships << Puppet::Relationship.new(dependency, self)
-#       end
-#     end
-# #    end
-#   end
 end
